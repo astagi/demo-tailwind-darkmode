9dd1811a6d0addbb65418ebc54e9243bbc4ab360
Andrea Stagi <stagi.andrea@gmail.com>
2020-11-24 23:46:09 +0100

    Fix 100vh bug on iOS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss')('./tailwind.config.js');
+const vhfix = require('postcss-100vh-fix');
 
 module.exports = {
   plugins: [
+    vhfix,
     tailwindcss,
     autoprefixer,
   ]
