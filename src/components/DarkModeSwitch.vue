e7a25f1a3e2a245b5154ee7fd7594527c6359382
Andrea Stagi <stagi.andrea@gmail.com>
2020-11-24 22:14:31 +0100

    First import

---
+++ src/components/DarkModeSwitch.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="text-gray-400 flex items-center">
+    <o-icon pack="fas" icon="sun" size="medium" :class="{'text-highlight-background' : !darkMode}"></o-icon>
+    <o-switch class="m-2" v-model="darkMode"></o-switch>
+    <o-icon pack="fas" icon="moon" size="medium" :class="{'text-highlight-background-dark' : darkMode}"></o-icon>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "DarkModeSwitch",
+  data() {
+    return {
+      darkMode: null,
+    };
+  },
+  mounted() {
+    this.darkMode =
+      localStorage.theme == "dark"
+        ? true
+        : localStorage.theme == "light"
+        ? false
+        : window.matchMedia("(prefers-color-scheme: dark)").matches;
+  },
+  watch: {
+    darkMode(isDarkNow, wasDarkBefore) {
+      if (wasDarkBefore !== null) {
+        localStorage.theme = isDarkNow ? "dark" : "light";
+      }
+      let html = document.querySelector("html");
+      if (isDarkNow) {
+        html.classList.add("dark");
+      } else {
+        html.classList.remove("dark");
+      }
+    },
+  },
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {Icon, Switch} from '@oruga-ui/oruga';
+import '@oruga-ui/oruga/dist/oruga.css';
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
+import './assets/tailwind.css';
+
 Vue.config.productionTip = false
+
+library.add(faSun);
+library.add(faMoon);
+
+Vue.use(Icon);
+Vue.use(Switch);
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+dom.watch();
